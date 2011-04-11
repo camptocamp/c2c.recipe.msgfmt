327817d351341a5a2914998816dce659fe8901a8
fredj <frederic.junod@camptocamp.com>
2011-04-11 09:28:18 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'c2c.recipe.msgfmt',
-    version = '0.1',
+    version = '0.2.1',
     license = 'MIT License',
 
     author  = 'Frederic Junod',
